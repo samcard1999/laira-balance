a6f32466c1094c0af96b6eec03117019267b9846
Samuel Cardenas <samcard1999@gmail.com>
2025-10-23 17:48:01 -0400

    ss

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import lenis from "astro-lenis";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [lenis(), react(), tailwind(),],
   i18n: {
     locales: ["es", "en"],
     defaultLocale: "es",
-  }
+  },
+
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+
+  ],
 }
 
